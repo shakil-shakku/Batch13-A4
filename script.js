a9959cb860a79b6ca14137520eaf127337de4a77
mdshakil805262 <mdshakil805262@gmail.com>
2026-02-23 19:38:50 +0600

    job card added

---
+++ script.js
@@ -0,0 +1,82 @@
+const jobs =[
+    {
+        id:1, 
+        company:"DataSoft Systems Bangladesh Limited", 
+        position:"Software Development", 
+        location:"Shymoli,Dhaka,Bangladesh", 
+        type:"Full-Time",
+        salary:"25,000 – 120,000 BDT per month",
+        description:"One of the leading local software companies in Bangladesh.",
+        status:"All"
+    },
+    {
+        id:2, 
+        company:" TigerIT Bangladesh Limited", 
+        position:" IT & Biometric Solutions", 
+        location:"Banani,Dhaka,Bangladesh", 
+        type:"Full-Time",
+        salary:"30,000 – 100,000 BDT per month",
+        description:"Known for biometric and identity management systems.",
+        status:"All"
+    },
+    {
+        id:3, 
+        company:"Brain Station 23", 
+        position:"Software & Mobile App Development", 
+        location:"Mohakhali,Dhaka,Bangladesh", 
+        type:"contract",
+        salary:"25,000 – 150,000 BDT per month",
+        description:"A fast-growing company providing web, mobile, and enterprise software solutions to international clients.",
+        status:"All"
+    },
+    {
+        id:4, 
+        company:"BJIT Limited", 
+        position:"IT Outsourcing", 
+        location:"Baridhara,Dhaka,Bangladesh", 
+        type:"Full-Time",
+        salary:"$120,000/year",
+        description:"Works mainly with Japanese and global clients. Provides offshore software development and QA services.",
+        status:"All"
+    },
+    {
+        id:5, 
+        company:"REVE Systems", 
+        position:"Telecom Software", 
+        location:"Gulshan,Dhaka,Bangladesh", 
+        type:"Remote",
+        salary:"25,000 – 110,000 BDT per month",
+        description:"Develops telecom software such as VoIP, messaging platforms, and billing systems used worldwide.",
+        status:"All"
+    },
+    {
+        id:6, 
+        company:"Enosis Solutions", 
+        position:"Software Development", 
+        location:"khilkhet,Dhaka,Bangladesh", 
+        type:"Full-Time",
+        salary:"30,000 – 140,000 BDT per month",
+        description:"Provides high-quality software development services for North American and international clients.",
+        status:"All"
+    },
+    {
+        id:7, 
+        company:"Selise Digital Platforms", 
+        position:"Enterprise & Cloud Software", 
+        location:"Dhanmondi,Dhaka,Bangladesh", 
+        type:"contract",
+        salary:"40,000 – 180,000 BDT per month",
+        description:"DFocuses on digital transformation and enterprise solutions using modern cloud technologies.",
+        status:"All"
+    },
+    {
+        id:8, 
+        company:"Kaz Software Limited", 
+        position:"Web & Enterprise Application Development", 
+        location:"Shantinagar,Dhaka,Bangladesh", 
+        type:"Remote",
+        salary:"30,000 – 120,000 BDT per month",
+        description:" Builds custom web applications and enterprise systems mainly for USA-based clients.",
+        status:"All"
+    },
+]
